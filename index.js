c97f0a105a2ba46c40b56271babf1e2cb2c0f3cc
Zineb <Zineb@DESKTOP-7FM9LA8>
2025-06-21 02:19:46 +0200

    Ajout affichage enrichi avec cards et images

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post('/webhook', async (req, res) => {
       }
 
       const sql = `
-        SELECT l.name, l.description, i.url
+        SELECT l.name, l.description, i.image_url
         FROM ${table} a
         JOIN location l ON a.id_location = l.id_location
         LEFT JOIN image i ON i.id_location = l.id_location
